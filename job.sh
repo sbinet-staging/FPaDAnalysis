bd905664a804067a11f9229206e13cdc919f6fe5
David Blyth <dblyth@anl.gov>
2017-06-29 12:41:38 -0500

    Modified job.sh wrapper script and made a better default nEventsPerRun

--- job.sh
+++ job.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/bash -l
 
-bash --login -c "mkdir -p input/$1 output; \
-			mv $(basename $2) input/$1/"
+mkdir -p input/$1 output
+mv $(basename $2) input/$1/
 
-bash --login -c "make"
+make
 
-bash --login -c "rm output/$1/*_tracking.slcio output/$1/*_pandora.slcio"
+rm output/$1/*_tracking.slcio output/$1/*_pandora.slcio
 
